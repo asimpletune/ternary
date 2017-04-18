47cb4dd42945596a7005a84930151df50bde44a0
Spencer Scorcelletti <asimpletune@users.noreply.github.com>
2017-04-17 20:42:41 -0700

    Add decoder, and increment/decrement operators

--- main.js
+++ main.js
@@ -31,3 +31,58 @@ Unary.not = function(operand) {
 Unary.identity = function(operand) {
   return operand
 }
+
+/**
+ * Decoder
+ */
+var Decode = new Object()
+Decode.true = function(operand) {
+  if (operand === true) {
+    return true
+  } else {
+    return false
+  }
+}
+
+Decode.false = function(operand) {
+  if (operand === false) {
+    return true
+  } else {
+    return false
+  }
+}
+
+Decode.undefined = function(operand) {
+  if (operand === undefined) {
+    return true
+  } else {
+    return false
+  }
+}
+
+Unary.decode = Decode
+
+/**
+ * Increment/Decrement
+ */
+Unary.increment = function(operand) {
+  if (operand === false) {
+    return undefined
+  } else if (operand === undefined) {
+    return true
+  } else if (operand === true) {
+    return false
+  }
+}
+
+Unary.decrement = function(operand) {
+  if (operand === false) {
+    return true
+  } else if (operand === undefined) {
+    return false
+  } else if (operand === true) {
+    return undefined
+  }
+}
+
+module.exports = Unary
